e9f733dee6ba4e5e8111e4dd802b19f8a82c0429
Geoff Doty <n2geoff@gmail.com>
2013-08-15 00:01:26 -0400

    added standard script assests and linked up sphere

---
+++ Assets/Scripts/LookAt.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var target: Transform;
+var distance: float;
+
+function Start () {
+	distance = 5;
+}
+
+function Update () {
+	transform.LookAt(target);
+	
+	transform.position.z = target.position.z - distance;
+    //transform.position.y = target.position.y;
+    //transform.position.x = target.position.x;
+}
